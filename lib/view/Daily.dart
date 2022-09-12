099f7cf43afbc9232fa1649e4f73bcf3c79a7ad5
ahmedkhattab <khattabahmed723@gmail.com>
2022-09-12 16:14:16 +0200

    f11 commit

--- lib/view/Daily.dart
+++ lib/view/Daily.dart
@@ -1,13 +1,59 @@
 
 import 'package:flutter/material.dart';
-
-class DailyPage extends StatelessWidget {
+class DailyPage extends StatefulWidget {
   static const String routeName="/Daily";
+  @override
+  State<DailyPage> createState() => _DailyPageState();
+}
+class _DailyPageState extends State<DailyPage> {
+  int currIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(child: Text("hello "),),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor:Color.fromRGBO(217, 217, 217, .5),
+        elevation: 0,
+        selectedFontSize: 20,
+        unselectedFontSize: 15,
+        selectedItemColor: Color.fromRGBO(22, 178, 66, 1),
+        unselectedItemColor: Colors.black54,
+        selectedIconTheme:IconThemeData(
+          color: Color.fromRGBO(22, 178, 66, 1),
+          size: 38,
+        ),
+        unselectedIconTheme:IconThemeData(
+          color: Colors.black54,
+          size: 28,
+        ),
+        currentIndex: currIndex,
+        onTap: (int i){
+          setState(() {
+            currIndex=i;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month_outlined,),
+            label: "Dialy",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add,),
+              label: "Add"
+
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month_outlined,),
+              label: "lose",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month_outlined),
+            label: "Over"
+          )
+        ],
+      ),
     );
   }
 }
